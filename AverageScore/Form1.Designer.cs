dc98090be207903e31ccf2c57a44a1f2726ae78f
Chris <mx0756wt@go.minnstate.edu>
2021-09-09 09:40:49 -0500

    Average Test Score (Ex1E) is finished

---
+++ AverageScore/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace AverageScore
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTestOne = new System.Windows.Forms.TextBox();
+            this.txtTestThree = new System.Windows.Forms.TextBox();
+            this.txtTestTwo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAverage = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Test 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Test 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Test 3:";
+            // 
+            // txtTestOne
+            // 
+            this.txtTestOne.Location = new System.Drawing.Point(106, 22);
+            this.txtTestOne.Name = "txtTestOne";
+            this.txtTestOne.Size = new System.Drawing.Size(100, 26);
+            this.txtTestOne.TabIndex = 0;
+            this.txtTestOne.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtTestThree
+            // 
+            this.txtTestThree.Location = new System.Drawing.Point(106, 129);
+            this.txtTestThree.Name = "txtTestThree";
+            this.txtTestThree.Size = new System.Drawing.Size(100, 26);
+            this.txtTestThree.TabIndex = 2;
+            this.txtTestThree.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtTestTwo
+            // 
+            this.txtTestTwo.Location = new System.Drawing.Point(106, 74);
+            this.txtTestTwo.Name = "txtTestTwo";
+            this.txtTestTwo.Size = new System.Drawing.Size(100, 26);
+            this.txtTestTwo.TabIndex = 1;
+            this.txtTestTwo.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Average:";
+            // 
+            // txtAverage
+            // 
+            this.txtAverage.Location = new System.Drawing.Point(106, 183);
+            this.txtAverage.Name = "txtAverage";
+            this.txtAverage.ReadOnly = true;
+            this.txtAverage.Size = new System.Drawing.Size(100, 26);
+            this.txtAverage.TabIndex = 3;
+            this.txtAverage.TabStop = false;
+            this.txtAverage.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(290, 230);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 31);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(162, 230);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(100, 31);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(32, 230);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 31);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(232, 22);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(178, 187);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.TabStop = false;
+            this.richTextBox1.Text = "Average Test Score:\n\nPlug in the 3 tests you had and find out the average on test" +
+    "s you taken and find out how to improve that score.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 273);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtAverage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTestTwo);
+            this.Controls.Add(this.txtTestThree);
+            this.Controls.Add(this.txtTestOne);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Average Test Score";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTestOne;
+        private System.Windows.Forms.TextBox txtTestThree;
+        private System.Windows.Forms.TextBox txtTestTwo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAverage;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ AverageScore/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AverageScore
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            txtAverage.Text = ((Convert.ToDecimal(txtTestOne.Text) +
+                Convert.ToDecimal(txtTestTwo.Text) +
+                Convert.ToDecimal(txtTestThree.Text)) / 3).ToString("0.000");
+
+            btnReset.Focus();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtAverage.Text = "";
+            txtTestOne.Text = "";
+            txtTestTwo.Text = "";
+            txtTestThree.Text = "";
+
+            txtTestOne.Focus();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
